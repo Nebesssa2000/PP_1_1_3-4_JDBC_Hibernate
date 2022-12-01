5880e25cc970bc90c71d10697a4426742bfa9dc5
Gabbasov Victor <victtss@gmail.com>
2022-12-02 01:33:18 +0300

    C9 " добавил вывод в консоль в main "

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,5 @@
 package jm.task.core.jdbc;
 
-
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
@@ -9,10 +8,14 @@ public class Main {
         UserServiceImpl userServiceImpl = new UserServiceImpl();
         userServiceImpl.createUsersTable();
         userServiceImpl.saveUser("Jack", "Black", (byte)30);
+        System.out.println("User c именем Jack добавлен в базу данных.");
         userServiceImpl.saveUser("Joe", "White", (byte)35);
+        System.out.println("User c именем Joe добавлен в базу данных.");
         userServiceImpl.saveUser("Bob", "Ferry", (byte)40);
+        System.out.println("User c именем Bob добавлен в базу данных.");
         userServiceImpl.saveUser("Natan", "Jones", (byte)45);
-        userServiceImpl.getAllUsers();
+        System.out.println("User c именем Natan добавлен в базу данных.");
+        System.out.println(userServiceImpl.getAllUsers().toString());
         userServiceImpl.cleanUsersTable();
         userServiceImpl.dropUsersTable();
     }
